46aec2501d3aed0806bcafbc2bb6f887b590f531
Jungwon Oh <jungwon.oh87@gmail.com>
2020-07-22 22:00:10 -0700

    collection-item component added and it displays data

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
@@ -6,9 +7,11 @@ const CollectionPreview = ({ title, items }) => {
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map((i) => {
-          return <div key={i.id}>{i.name}</div>;
-        })}
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherItemProps }) => {
+            return <CollectionItem key={id} {...otherItemProps} />;
+          })}
       </div>
     </div>
   );
