2cc2893b967cc733e80a5f015dee1fd7bc91eb1b
Jungwon Oh <jungwon.oh87@gmail.com>
2020-07-25 20:22:28 -0700

    header added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -8,6 +9,7 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ name, imageUrl, price }) => {
   return (
     <div className="collection-item">
       <div
@@ -11,7 +11,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
       />
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
     </div>
   );
